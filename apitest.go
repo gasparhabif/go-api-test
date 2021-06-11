7b21cc4b5cf5f28d5f6814c157bfd71b76b216b3
Gaspar Habif <gasparhabif@gmail.com>
2021-06-11 20:01:51 -0300

    Finalized CRUD operations

--- apitest.go
+++ apitest.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strings"
@@ -33,8 +34,12 @@ func handleRequests() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", homePage)
-	router.HandleFunc("/articles", allArticles)
-	router.HandleFunc("/article/{id}", singleArticle)
+	router.HandleFunc("/articles", allArticles).Methods("GET")
+	router.HandleFunc("/article/{id}", singleArticle).Methods("GET")
+	router.HandleFunc("/article", addArticle).Methods("POST")
+	router.HandleFunc("/article/{id}", removeArticle).Methods("DELETE")
+	router.HandleFunc("/article/{id}", updateArticle).Methods("PUT")
+
 
 	log.Fatal(http.ListenAndServe(PORT, router))
 }
@@ -45,25 +50,65 @@ func printCurrentEndpoint(endpoint string) {
 	}
 }
 
-func homePage(w http.ResponseWriter, r *http.Request){
-	printCurrentEndpoint(r.URL.String())
+func homePage(w http.ResponseWriter, request *http.Request){
+	printCurrentEndpoint(request.URL.String())
 	fmt.Fprintf(w, "Holis")
 }
 
-func allArticles(w http.ResponseWriter, r *http.Request) {
-	printCurrentEndpoint(r.URL.String())
+func allArticles(w http.ResponseWriter, request *http.Request) {
+	printCurrentEndpoint(request.URL.String())
 	json.NewEncoder(w).Encode(Articles)
 }
 
-func singleArticle(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
+func singleArticle(w http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
 	key := vars["id"]
 
-	printCurrentEndpoint(r.URL.String())
+	printCurrentEndpoint(request.URL.String())
 
 	for _, article := range Articles {
 		if article.Id == key {
 			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
+func addArticle(w http.ResponseWriter, request *http.Request) {
+	reqBody, _ := ioutil.ReadAll(request.Body)
+	var article Article
+	json.Unmarshal(reqBody, &article)
+	Articles = append(Articles, article)
+
+	json.NewEncoder(w).Encode(article)
+}
+
+func removeArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for index, article := range Articles {
+		if article.Id == id {
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+}
+
+func updateArticle(w http.ResponseWriter, request *http.Request) {
+	vars := mux.Vars(request)
+	id := vars["id"]
+
+	reqBody, _ := ioutil.ReadAll(request.Body)
+	var updatedArticle Article
+	json.Unmarshal(reqBody, &updatedArticle)
+
+	for index, article := range Articles {
+		if article.Id == id {
+			var articleToUpdate = Articles[index]
+			articleToUpdate.Title = updatedArticle.Title
+			articleToUpdate.Content = updatedArticle.Content
+			articleToUpdate.Description = updatedArticle.Description
+			Articles[index] = articleToUpdate
+			json.NewEncoder(w).Encode(articleToUpdate)
 		}
 	}
 }
